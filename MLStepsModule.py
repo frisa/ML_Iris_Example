3fb6dd6de792430d791d2d45ffa3a589379207d3
Jan <fridrichovsky@gmail.com>
2019-01-06 18:52:20 +0100

    make prediction

--- MLStepsModule.py
+++ MLStepsModule.py
@@ -84,3 +84,11 @@ ax = fig.add_subplot(111)
 plt.boxplot(results)
 ax.set_xticklabels(names)
 plt.show()
+
+# Make predictions on validation dataset
+knn = KNeighborsClassifier()
+knn.fit(X_train, Y_train)
+predictions = knn.predict(X_validation)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
